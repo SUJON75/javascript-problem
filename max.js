2d6e46690d530dca1592d0ab4f1be1297854de39
MD. JAHIRUL ISLAM <37324502+SUJON75@users.noreply.github.com>
2020-04-10 11:56:31 +0600

    fast commit

---
+++ max.js
@@ -0,0 +1,30 @@
+var business = 850;
+var minister = 450;
+var sochib = 950;
+
+var max = Math.max(business, minister, sochib)
+console.log(max);
+
+
+
+
+
+/** 
+
+if(business > minister){
+    if(business > sochib){
+        console.log("Business is bigger");
+    }
+    else{
+        console.log("sochib is bigger");
+    }
+}
+else{
+    if(minister > sochib){
+        console.log("Minister is bigger");
+    }
+    else{
+        console.log("sochib is bigger");
+    }
+}
+*/
